15fa0c45f2811d1175f740264fd6b35975605f3a
gelrond <121748300+gelrond@users.noreply.github.com>
2023-01-05 11:56:35 +0000

    Small tweak

--- README.md
+++ README.md
@@ -4,7 +4,7 @@
 
 <p>Simple test to test post processing and lighting using three.js</p>
 
-<a href="https://ibb.co/XWRqdMk"><img src="https://i.ibb.co/10WkpjJ/screenshot-1.png" alt="screenshot-1" border="0"></a>
+<a href="https://ibb.co/xjhfZcQ"><img src="https://i.ibb.co/fpMnJK7/screenshot-1.png" alt="screenshot-1" border="0"></a>
 
 <h3>Installation</h3>
 
--- main.ts
+++ main.ts
@@ -1,5 +1,4 @@
 // ********************************************************************************************************************
-// ********************************************************************************************************************
 import { Color, MathUtils, Mesh, MeshStandardMaterial, PerspectiveCamera, PointLight, Scene, SphereGeometry, Vector2, WebGLRenderer } from 'three';
 // ********************************************************************************************************************
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
@@ -15,7 +14,7 @@ import { UnrealBloomPass } from 'three/examples/jsm/postprocessing/UnrealBloomPa
 // scene & renderer
 // ********************************************************************************************************************
 const scene = new Scene();
-scene.background = new Color('#010101');
+scene.background = new Color('#050505');
 const renderer = new WebGLRenderer({ antialias: true });
 renderer.setSize(800, 600);
 
@@ -31,7 +30,7 @@ new OrbitControls(camera, renderer.domElement);
 // ********************************************************************************************************************
 const composer = new EffectComposer(renderer);
 composer.addPass(new RenderPass(scene, camera));
-const unrealBloomPass = new UnrealBloomPass(new Vector2(1024, 1024), 4, 1, 0);
+const unrealBloomPass = new UnrealBloomPass(new Vector2(800, 600), 4, 1, 0);
 composer.addPass(unrealBloomPass);
 
 // ********************************************************************************************************************
@@ -40,18 +39,21 @@ composer.addPass(unrealBloomPass);
 const meshes: Mesh[][] = [];
 const size = 8;
 const sizeHalf = size / 2;
+const sizeDouble = size * 2;
 
 for (var x = 0; x < size; x++) {
 
     meshes[x] = [];
 
     for (var y = 0; y < size; y++) {
-        const geometry = new SphereGeometry(size / 2);
-        const material = new MeshStandardMaterial({ color: '#ffa000', roughness: 0.2, transparent: false, opacity: 0.5 });
+
+        const geometry = new SphereGeometry(sizeHalf);
+        const material = new MeshStandardMaterial({ color: '#ff8010', roughness: 0.2, transparent: false, opacity: 0.5 });
         const mesh = new Mesh(geometry, material);
-        mesh.position.setX((x - sizeHalf) * (size + 0));
-        mesh.position.setY((y - sizeHalf) * (size + 0));
+        mesh.position.setX((x - sizeHalf) * size);
+        mesh.position.setY((y - sizeHalf) * size);
         meshes[x][y] = mesh;
+
         mesh.userData.offset = 0;
         mesh.userData.speed = MathUtils.randFloat(0.05, 0.2);
         mesh.userData.direction = MathUtils.randInt(0, 1);
@@ -59,25 +61,29 @@ for (var x = 0; x < size; x++) {
 
         if ((x % 2) === 0 && (y % 2) === 0) {
             const colour = new Color(MathUtils.randFloat(0, 1), MathUtils.randFloat(0, 1), MathUtils.randFloat(0, 1));
-            const point = new PointLight(colour, 0.05);
-            point.position.set(mesh.position.x, mesh.position.y, size * 2);
+            const point = new PointLight(colour, 0.025);
+            point.position.set(mesh.position.x, mesh.position.y, sizeDouble);
             mesh.children.push(point);
         }
     }
 }
+
+// ********************************************************************************************************************
+// update meshes
+// ********************************************************************************************************************
 function updateMeshes() {
     for (var x = 0; x < size; x++) {
         for (var y = 0; y < size; y++) {
             const mesh = meshes[x][y];
             if (mesh.userData.direction === 0) {
                 mesh.userData.offset += mesh.userData.speed;
-                if (mesh.userData.offset > size * 3) {
+                if (mesh.userData.offset > sizeDouble) {
                     mesh.userData.direction = 1;
                 }
             }
             if (mesh.userData.direction === 1) {
                 mesh.userData.offset -= mesh.userData.speed;
-                if (mesh.userData.offset < 0 - size * 3) {
+                if (mesh.userData.offset < 0 - sizeDouble) {
                     mesh.userData.direction = 0;
                 }
             }
